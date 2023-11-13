75f79ac3a8b5f88fab86cef3d2492594899d0b93
BBIYAKYEE7 <dadalgojr@gmail.com>
2023-11-13 12:19:08 +0900

    Codes

--- Comsigan/ViewController.swift
+++ Comsigan/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let url = URL(string: "http://comci.net:4082/st")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
-
+    // 웹뷰가 웹 컨텐츠를 모두 로드한 후에 호출되는 메서드
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // 웹 페이지의 모든 요소의 너비를 화면 너비의 절반으로 설정하는 JavaScript 코드
+        let script = """
+        document.body.style.zoom = "0.5";
+        """
+        // JavaScript 코드를 실행
+        webView.evaluateJavaScript(script, completionHandler: nil)
+    }
 }
-
